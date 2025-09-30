6c877cfa1e4ead5b8e123dabead74fdf63c733b5
Dhanush1374949 <dhanushkumar0782@gmail.com>
2025-09-30 14:48:17 +0530

    fix add some in this

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 
+// https://vite.dev/config/
 export default defineConfig({
+
   plugins: [
-     tailwindcss(),
+        tailwindcss(),
+
     react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:5000", // Your backend server
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
-});
+    base: "/Morden-portfolio/", // 👈 required for GitHub Pages
+
+})
