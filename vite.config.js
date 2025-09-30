47d0662b263ee61df3bf64b2dc7a392ad6faf2a3
Dhanush1374949 <dhanushkumar0782@gmail.com>
2025-09-30 14:35:29 +0530

    fix add some in this

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
-
   plugins: [
-        tailwindcss(),
-
+     tailwindcss(),
     react()],
-})
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000", // Your backend server
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
