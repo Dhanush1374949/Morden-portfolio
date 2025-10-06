4f5b9b3ad270d8b4b202ea947d1f4753d3bce129
Dhanush1374949 <dhanushkumar0782@gmail.com>
2025-10-06 15:56:00 +0530

    fix add some in this

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,13 +1,14 @@
+import { IoCall, IoMail } from "react-icons/io5";
+
+
 export default function Contact() {
   return (
     <section id="contact" className="py-16 max-w-5xl mx-auto px-4 text-center">
       <h2 className="text-3xl font-bold mb-6">Contact Me</h2>
-      <form className="max-w-md mx-auto space-y-4">
-        <input type="text" placeholder="Your Name" className="w-full p-3 rounded-md border dark:bg-gray-900 dark:text-white"/>
-        <input type="email" placeholder="Your Email" className="w-full p-3 rounded-md border dark:bg-gray-900 dark:text-white"/>
-        <textarea placeholder="Your Message" className="w-full p-3 rounded-md border dark:bg-gray-900 dark:text-white"></textarea>
-        <button className="bg-primary text-white px-6 py-3 rounded-full shadow hover:opacity-90 transition">Send</button>
-      </form>
+      <div className="max-w-md mx-auto space-y-7">
+        <h2><a href="">dhanushmurugan30752gmail.com</a></h2>
+        <h2>9361610642</h2>
+      </div>
     </section>
   );
 }
