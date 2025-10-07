f21870e0c1733fe04cc937faaecf0237c4bd7842
Dhanush1374949 <dhanushkumar0782@gmail.com>
2025-10-07 16:37:22 +0530

    fix add some in this

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Projects from "./components/Projects";
-import Resume from "./components/Resume";
+// import Resume from "./components/Resume";
 import Contact from "./components/Contact";
 import SocialLinks from "./components/SocialLinks";
 import Footer from "./components/Footer";
@@ -27,7 +27,7 @@ export default function App() {
       <About />
       <Skills/>
       <Projects />
-      <Resume />
+      {/* <Resume /> */}
       <Contact />
       <SocialLinks />
       <Footer/>
--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,20 +1,20 @@
-import resume from "/src/assets/myresume.jpg";
+// import resume from "/src/assets/myresume.jpg";
 
 
 
-export default function Resume() {
-  return (
-    <section id="resume" className=" py-16 max-w-5xl mx-auto px-4 text-center">
-      <h2 className="text-3xl font-bold mb-6">Resume</h2>
-<div className="flex justify-center items-center m-15">
-      <img src={resume} alt="My Resume" className="resume rounded-lg shadow-lg" />
-    </div>      <p className="text-lg mb-6">Download my resume to learn more about my skills.</p>
-      <button
-        onClick={() => window.print()}
-        className="bg-primary text-white px-6 py-3 rounded-full shadow hover:opacity-90 transition"
-      >
-        Print / Save Resume
-      </button>
-    </section>
-  );
-}
+// export default function Resume() {
+//   return (
+//     <section id="resume" className=" py-16 max-w-5xl mx-auto px-4 text-center">
+//       <h2 className="text-3xl font-bold mb-6">Resume</h2>
+// <div className="flex justify-center items-center m-15">
+//       <img src={resume} alt="My Resume" className="resume rounded-lg shadow-lg" />
+//     </div>      <p className="text-lg mb-6">Download my resume to learn more about my skills.</p>
+//       <button
+//         onClick={() => window.print()}
+//         className="bg-primary text-white px-6 py-3 rounded-full shadow hover:opacity-90 transition"
+//       >
+//         Print / Save Resume
+//       </button>
+//     </section>
+//   );
+// }
